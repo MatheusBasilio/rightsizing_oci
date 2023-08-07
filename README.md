25a53e113816ac857825cf40ed2bfac761b18139
Matheus Basilio <matheus.basilio@enkel.com.br>
2023-08-07 11:21:17 -0300

    Add Files

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# rightsizing_oci
+<h3 align="center">OCI Metrics Aggregator</h3>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](/LICENSE)
+
+</div>
+
+---
+
+<p align="center"> This script gathers CPU and Memory utilization metrics from Oracle Cloud Infrastructure (OCI) instances and outputs the results to a CSV file for further analysis.
+    <br> 
+</p>
+
+## 📝 Table of Contents
+
+- [About](#about)
+- [Prerequisites](#prerequisites)
+- [Usage](#usage)
+- [Output](#output)
+- [Contributing](../CONTRIBUTING.md)
+- [Authors](#authors)
+- [License](#license)
+
+## 🧐 About <a name="about"></a>
+
+This script is designed to gather CPU and Memory utilization metrics from OCI instances using the OCI Command Line Interface (CLI). It calculates the average CPU and Memory utilization for each instance over the past 30 days and outputs the results in CSV format.
+
+## 🛠️ Prerequisites <a name="prerequisites"></a>
+
+Before using this script, make sure you have the OCI CLI installed and properly configured with the necessary credentials. You can follow the [OCI CLI documentation](https://docs.oracle.com/en-us/iaas/Content/API/Concepts/cliconcepts.htm) for installation and setup instructions.
+
+## 🚀 Usage <a name="usage"></a>
+
+1. Clone this repository to your local machine.
+2. Open a terminal and navigate to the repository's directory.
+3. Make sure the `oci_metrics.sh` script has executable permissions. If not, you can use the command: `chmod +x oci_metrics.sh`.
+4. Run the script using the command: `bash oci_metrics.sh > metrics.csv`.
+
+The script will gather CPU and Memory utilization metrics for each instance across all compartments you have access to and output the results to the `metrics.csv` file.
+
+## 📄 Output <a name="output"></a>
+
+The output CSV file will have the following format:
+
+INSTANCE ID, CPU Utilization, Memory Utilization
+
+
+You can use this CSV file for further analysis and reporting.
+
+## ✍️ Authors <a name="authors"></a>
+
+- Your Name - [@MatheusBasilio](https://github.com/MatheusBasilio)
+
+## 📄 License <a name="license"></a>
+
+This project is licensed under the MIT License - see the [LICENSE](/LICENSE) file for details.
---
+++ oci_metrics.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+compartments=$(oci iam compartment list --access-level any --compartment-id-in-subtree true --query "data[*].id" --raw-output)
+echo "INSTANCE ID, CPU Utilization, Memory Uitlization"
+# Obtendo a data atual em formato Unix timestamp
+current_time=$(date +%s)
+
+# Calculando o Unix timestamp de 30 dias atrás
+start_time=$((current_time - 30*24*60*60))
+
+# Convertendo os Unix timestamps para o formato desejado
+start_time_formatted=$(date -u -d @$start_time +'%Y-%m-%dT%H:%M:%SZ')
+end_time_formatted=$(date -u -d @$current_time +'%Y-%m-%dT%H:%M:%SZ')
+
+for compartment_id in $compartments; do
+    compartment_id=$(echo "$compartment_id" | sed 's/,//g' | sed 's/"//g') 
+    instances=$(oci compute instance list --compartment-id $compartment_id --query "data[*].id" --raw-output)
+
+    for instance_id in $instances; do
+    instance_id=$(echo "$instance_id" | sed 's/,//g')
+    # Executar o comando e armazenar a saída JSON em uma variável
+    command_output_memory=$(oci monitoring metric-data summarize-metrics-data --compartment-id $compartment_id --namespace oci_computeagent --query-text "MemoryUtilization[1d]{resourceId=$instance_id}.max()" --start-time $start_time_formatted --end-time $end_time_formatted)
+    command_output_cpu=$(oci monitoring metric-data summarize-metrics-data --compartment-id $compartment_id --namespace oci_computeagent --query-text "CpuUtilization[1d]{resourceId=$instance_id}.max()" --start-time $start_time_formatted --end-time $end_time_formatted)
+    if [ "$instance_id" != "[" ] && [ "$instance_id" != "]" ]; then
+    # Extrair todos os valores "value" do JSON e calcular a média
+        average_memory=0
+        count_memory=0
+        total_memory=0
+        values=$(echo "$command_output_memory" | jq -r '.data[0]."aggregated-datapoints"[] | .value')
+        if [ -n "$values" ]; then
+            while IFS= read -r value; do
+            total_memory=$(echo "$total_memory + $value" | bc)
+            count_memory=$((count_memory + 1))
+            done <<< "$values"
+        fi
+
+        if [ $count_memory -gt 0 ]; then
+            average_memory=$(echo "scale=2; $total_memory / $count_memory" | bc)
+        fi
+
+        average_cpu=0
+        count_cpu=0
+        total_cpu=0
+
+        values=$(echo "$command_output_cpu" | jq -r '.data[0]."aggregated-datapoints"[] | .value')
+        if [ -n "$values" ]; then
+            while IFS= read -r value; do
+            total_cpu=$(echo "$total_cpu + $value" | bc)
+            count_cpu=$((count_cpu + 1))
+            done <<< "$values"
+        fi
+        if [ $count_cpu -gt 0 ]; then
+            average_cpu=$(echo "scale=2; $total_cpu / $count_cpu" | bc)
+        fi
+        echo "$instance_id, $average_cpu, $average_memory"
+        
+    fi
+    done
+done
